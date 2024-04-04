35462f50654b591ae0c82bffa288f2aee5545371
eduardoND9 <eduardocristal23@gmail.com>
2024-04-04 18:09:02 +0200

    implementacion de la funcionalidad

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,2 +1,30 @@
-const display=document.querySelector("#display");
-const conevertir=document.querySelector("convertir")
+const valor=document.getElementById('valor');
+const tDolar=document.getElementById('tDolar');
+const tSol=document.getElementById('tSol');
+
+// CREAR CONSTANTES DE LOS INPUTS DE RESULTADOS (SOLES Y DOLARES)
+const Rdolar =document.getElementById('resultadoD');
+const Rsol =document.getElementById('resultadoS');
+
+const botonConvertir=document.getElementById('convertir');
+
+
+function convertir() {
+    
+    let montoConvertir = parseFloat(valor.value);
+    let tipoCambioDolar = parseFloat(tDolar.value);
+    let tipoCambioSol = parseFloat(tSol.value);
+    
+
+ 
+    
+    // REALIZAR EL CALCULO Y GUARDARLOS EN VARIABLES DE RESULTADOS
+    Rdolar.value = (montoConvertir*tipoCambioDolar).toFixed(2);
+    Rsol.value = (montoConvertir*tipoCambioSol).toFixed(2);
+
+}
+
+botonConvertir.addEventListener('click', convertir);
+
+
+
